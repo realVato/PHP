d8b9a3041bb6bd05a5604c19e8b8a7de166d47d8
realVato <44781061+realVato@users.noreply.github.com>
2020-01-04 23:18:44 +0200

    POST

--- GET_POST/index.php
+++ GET_POST/index.php
@@ -17,10 +17,19 @@
         <button type="submit">submit</button>
     </form>
 
+    <form action="index.php" method="post">
+        <input type="password" name="password">
+        <br>
+        <button type="submit">Submit</button>
+    </form>
+
 
     Your name: <?php echo $_GET['username'] ?>
     <br>
     Your age: <?php echo $_GET['age'] ?>
+    <br>
+    <br>
+    PASSWORD: <?php echo $_POST['password'] ?>
 </body>
 
 </html>
