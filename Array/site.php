378d41a4088f3ce7cbe23bdea57f5fe18cb2b7cc
realVato <44781061+realVato@users.noreply.github.com>
2020-01-05 11:37:26 +0200

    Arrays

---
+++ Array/site.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php 
+        $names = ['Dwight', 'Frank'];
+        $names2 = array('Dwight2', 'Frank2');
+
+        echo count($names);
+        echo $names2[1];
+    ?>
+
+</body>
+</html>
