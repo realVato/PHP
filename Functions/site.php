634592b1c752a2fc8cff94617184b60f0a56a8ea
realVato <44781061+realVato@users.noreply.github.com>
2020-01-05 13:20:31 +0200

    function

---
+++ Functions/site.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+    function sayHi($name, $age) {
+        echo "hello $name, your are $age years old, yes? <br>";
+    }
+
+    sayHi("Frank", 19);
+    sayHi("Susie", 18);
+
+?>
+
+    
+</body>
+</html>
