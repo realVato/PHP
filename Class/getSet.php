4005269b2142cdef3a1ac4a353024486d1d1fcdd
realVato <44781061+realVato@users.noreply.github.com>
2020-01-11 12:46:58 +0200

    Getters & Setters

---
+++ Class/getSet.php
@@ -0,0 +1,34 @@
+<?php
+
+    class Movie {
+        public $title;
+        private $rating;
+
+        function __construct($title, $rating)
+        {
+            $this->title = $title;
+            $this->setRating($rating);
+        }
+
+        function getRating() {
+          return $this->rating;
+        }
+
+        function setRating($rating) {
+            if($rating == 1 || $rating == 2 || $rating == 3) {
+                $this->rating = $rating;
+            } else {
+                $this->rating = "Not rated";
+            }
+        }
+    }
+
+
+
+    $movie = new Movie("Avengers", 2);
+    // 1, 2 , 3 (ratings)
+    
+    $movie->setRating(3);
+    echo $movie->getRating();
+
+?>
